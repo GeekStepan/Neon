ca1482df79f2fcca8ae98c3c4030b138907d6c8b
stepangorbach <stepan.gorbach@outlook.com>
2019-11-27 20:51:51 +0500

    lab4task2 compete

---
+++ lab4task2my.py
@@ -0,0 +1,18 @@
+def Armstrong(x):
+    bigList = [i for i in range(1, x+1)]
+    armstrongList = []
+    sumX = 0
+    for i in bigList:
+        sumX = 0
+        xNum = map(int, str(i))
+        xNum = list(xNum)
+        for j in range(len(xNum)):
+            sumX += int(xNum[j])
+        print("\nСумма цифр равна: ", sumX)
+        print("i = ", i, "sumX^n = ", sumX ** len(xNum))
+        if sumX ** len(xNum) == i:
+            armstrongList.append(i)
+    print(armstrongList)
+
+x = int(input("EBOY: "))
+Armstrong(x)
