66a5c80b5a36956675b6bbdc777d00a40850f50f
stepangorbach <stepan.gorbach@outlook.com>
2019-11-27 19:47:03 +0500

    lab4task1 complete :)

---
+++ lab4task1.py
@@ -0,0 +1,10 @@
+#Составить программу нахождения наименьшего общего кратного трех натуральных чисел.
+def number(num1, num2, num3):
+    numLs = [i for i in range(1,(num1*num2*num3)+1)]
+    for i in reversed(numLs):
+        if (i%num1 == 0) and (i%num2 == 0) and (i%num3 == 0):
+            print("наименьшое общее кратное: ", i)
+            break
+
+n1 = 10; n2 = 52; n3 = 9
+number(n1, n2, n3)
