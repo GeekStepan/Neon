497e2638683c14990f6ec26b7616009b3579fa35
stepangorbach <stepan.gorbach@outlook.com>
2019-12-08 19:53:08 +0500

    bug fix

--- lab5/lab5task3.py
+++ lab5/lab5task3.py
@@ -1,19 +1,20 @@
 from random import randint as random
 
 def lab3(x):
-    #listA = [10, 2, 32, 24, 8, 64, 7, 25, 23, 53]
-    listA =[100, 3, 0, 1, 32, 28, 512, 3, 87, 17, 6, 3, 97, 5]
+    #listA = [int(random(1, 1000)) for i in range(random(1, 100))]; n = random(1, 10000)
+    #listA = [10, 2, 32, 24, 8, 64, 7, 25, 23, 53]; n = x
+    listA =[100, 3, 0, 1, 32, 28, 512, 3, 87, 17, 6, 3, 97, 5]; n = x
+    print(listA, "\n")
     strA = str("0")
     test1 = 0; test2 = 0
-    n = x
     for i in range(len(listA)):
         for k in range(len(listA)):
             if i != k and abs(x - ((listA[i]+listA[k])/2)) < n:
                 n = abs(x - ((listA[i]+listA[k])/2))
                 test1 = listA[i]; test2 = listA[k]
                 strA = str(listA[i]) + str(" + ") + str(listA[k])
-                print("\n73 - ", (listA[i]+listA[k])/2, " = ", abs(x - ((listA[i]+listA[k])/2)))
-                print(strA, "среднее арифметическое: ", ((test1+test2)/2))
+                print(x, " - ", (listA[i]+listA[k])/2, " = ", abs(x - ((listA[i]+listA[k])/2)))
+                print(strA, "среднее арифметическое: ", ((test1+test2)/2), "\n")
 
-lab3(73)
+lab3(int(input("x = ")))
 
